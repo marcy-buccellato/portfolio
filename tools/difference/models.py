c84869b35b079a37b9cf5616412f1dab1a4e85a7
Marcy Petroccione <marcypetro@github.com>
2015-12-01 21:29:30 -0500

    Add Difference model, add difference app to installed apps, and add the database migration

--- tools/difference/models.py
+++ tools/difference/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Difference(models.Model):
+    number = models.IntegerField(default=0)
+    value = models.IntegerField(default=0)
+    occurrences = models.IntegerField(default=0)
+    date = models.DateTimeField('date created')
