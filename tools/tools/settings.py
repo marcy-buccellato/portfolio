bdcaeea887c139fa94808277c7320b15bd9d8bd1
Marcy Petroccione <marcypetro@github.com>
2015-12-06 14:11:33 -0500

    Format value in template

--- tools/tools/settings.py
+++ tools/tools/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'difference',
+
+    # Template tags
+    'django.contrib.humanize',
 )
 
 MIDDLEWARE_CLASSES = (
