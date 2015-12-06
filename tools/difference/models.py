fe13d3a757680f751b7c84d63e075e7afe80db78
Marcy Petroccione <marcypetro@github.com>
2015-12-06 13:39:54 -0500

    Reorganize tests. Add error handling. Add negative test

--- tools/difference/models.py
+++ tools/difference/models.py
@@ -1,6 +1,26 @@
-import math
+from math import pow
 
 from django.db import models
+
+
+def get_difference(number):
+    """
+    Get difference between the sum of the squares and the square of the sums
+    for a given number
+    """
+    if not isinstance(number, int):
+        return
+
+    sum_of_the_squares = 0
+    sum_of_the_numbers = 0
+
+    for x in range(1, number + 1):
+        sum_of_the_squares += pow(x, 2)
+        sum_of_the_numbers += x
+
+    square_of_the_sums = pow(sum_of_the_numbers, 2)
+
+    return square_of_the_sums - sum_of_the_squares
 
 
 class Difference(models.Model):
@@ -12,23 +32,6 @@ class Difference(models.Model):
     def save(self, force_insert=False, force_update=False, using=None,
              update_fields=None):
         if not self.value:
-            self.value = self.get_difference(self.number)
+            self.value = get_difference(self.number)
 
         return super(Difference, self).save()
-
-    @staticmethod
-    def get_difference(number):
-        """
-        Get difference between the sum of the squares and the square of the sums
-        for a given number
-        """
-        sum_of_the_squares = 0
-        sum_of_the_numbers = 0
-        for x in range(1, number + 1):
-            sum_of_the_squares += math.pow(x, 2)
-            sum_of_the_numbers += x
-
-        square_of_the_sums = math.pow(sum_of_the_numbers, 2)
-
-        return square_of_the_sums - sum_of_the_squares
-
